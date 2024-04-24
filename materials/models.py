431502490d3acf6743062e3fbf935ad7be1db716
evgeniy <evgeniy.igorevich9402@gmail.com>
2024-04-25 04:11:20 +0500

    добавил модели для Course, Lesson

---
+++ materials/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Название')
+    preview = models.ImageField(upload_to='course_previews/', verbose_name='Картинка', **NULLABLE)
+    description = models.TextField(max_length=300, verbose_name='Описание', **NULLABLE)
+
+    def __str__(self):
+        return f"{self.title}"
+
+    class Meta:
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
+
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Название')
+    description = models.TextField(max_length=300, verbose_name='Описание', **NULLABLE)
+    preview = models.ImageField(upload_to='lesson_previews/', verbose_name='Картинка', **NULLABLE)
+    video_link = models.URLField(verbose_name='Ссылка на видео', **NULLABLE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='Курс', **NULLABLE)
+
+    def __str__(self):
+        return f"{self.title}"
+
+    class Meta:
+        verbose_name = 'Урок'
+        verbose_name_plural = 'Уроки'
