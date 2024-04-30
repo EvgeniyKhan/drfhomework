49a0886ff78ef168b905966566b6f7f9991c7b07
evgeniy <evgeniy.igorevich9402@gmail.com>
2024-04-30 22:43:59 +0500

    добавил контроллеры

--- users/views.py
+++ users/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets, generics
+from rest_framework.filters import SearchFilter
 
-# Create your views here.
+
+from users.models import User, Payments
+from users.serliazers import UserSerializer, PaymentSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class UserListAPIView(generics.ListAPIView):
+    model = User
+    queryset = User.objects.all()
+
+
+class PaymentsCreateView(generics.CreateAPIView):
+    serializer_class = PaymentSerializer
+
+
+class PaymentsListAPIView(generics.ListAPIView):
+    serializer_class = PaymentSerializer
+    queryset = Payments.objects.all()
+    filter_backends = [SearchFilter, DjangoFilterBackend]
+    filterset_fields = ('paid_course', 'paid_lesson', 'payment_method')
+    ordering_fields = ('payment_amount',)
