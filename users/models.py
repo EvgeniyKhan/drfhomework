e95de861d5476592fdb1b86f941f2c8e1baeffaa
evgeniy <evgeniy.igorevich9402@gmail.com>
2024-04-30 22:43:00 +0500

    добавил новую модель Payment

--- users/models.py
+++ users/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from materials.models import Course, Lesson
+
 NULLABLE = {'blank': True, 'null': True}
 
 
@@ -20,3 +22,29 @@ class User(AbstractUser):
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
+
+
+class Payments(models.Model):
+    CASH = 'cash'
+    CARD = 'card'
+
+    PAYMENT_METHOD_CHOICES = (
+        (CASH, 'Наличные'),
+        (CARD, 'Банковский перевод')
+    )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь', **NULLABLE)
+    date_payment = models.DateField(auto_now=True, verbose_name='Дата оплаты')
+    paid_course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='Оплаченный курс', **NULLABLE)
+    paid_lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, verbose_name='Оплаченный урок', **NULLABLE)
+    payment_amount = models.PositiveIntegerField(verbose_name='Сумма оплаты')
+    payment_method = models.CharField(max_length=50, choices=PAYMENT_METHOD_CHOICES, default=CARD,
+                                      verbose_name='Способ оплаты')
+
+    def __str__(self):
+        return f'{self.user} - {self.paid_course if self.paid_course else self.paid_lesson}'
+
+    class Meta:
+        verbose_name = 'Платёж'
+        verbose_name_plural = 'Платежи'
+        ordering = ('user', 'date_payment')
