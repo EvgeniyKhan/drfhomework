832e696a7d3b8ea800f082262fdbe72cfe8fd7f3
evgeniy <evgeniy.igorevich9402@gmail.com>
2024-05-09 00:08:02 +0500

    добавил урлы

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.apps import UsersConfig
 from users.views import UserViewSet, UserListAPIView, PaymentsListAPIView, PaymentsCreateAPIView, UserCreateAPIView
@@ -12,7 +13,11 @@ app_name = UsersConfig.name
 
 urlpatterns = [
     path('users/', UserListAPIView.as_view(), name='users_list'),
-    path('users/create/', UserCreateAPIView.as_view(), name='users_create'),
+    path('register/', UserCreateAPIView.as_view(), name='register'),
+
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
     path('payments/', PaymentsListAPIView.as_view(), name='payments_list'),
     path('payments/create/', PaymentsCreateAPIView.as_view(), name='payments_create'),
 ]
