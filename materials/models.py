3491480299d9bdfbe4944a1d17cdc1486a822f7a
evgeniy <evgeniy.igorevich9402@gmail.com>
2024-05-09 00:05:00 +0500

    добавил в модели owner

--- materials/models.py
+++ materials/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 NULLABLE = {'blank': True, 'null': True}
@@ -7,6 +8,7 @@ class Course(models.Model):
     title = models.CharField(max_length=100, verbose_name='Название')
     preview = models.ImageField(upload_to='course_previews/', verbose_name='Картинка', **NULLABLE)
     description = models.TextField(max_length=300, verbose_name='Описание', **NULLABLE)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, verbose_name='Владелец', **NULLABLE)
 
     def __str__(self):
         return f"{self.title}"
@@ -22,6 +24,7 @@ class Lesson(models.Model):
     preview = models.ImageField(upload_to='lesson_previews/', verbose_name='Картинка', **NULLABLE)
     video_link = models.URLField(verbose_name='Ссылка на видео', **NULLABLE)
     course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='Курс', **NULLABLE)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, verbose_name='Владелец', **NULLABLE)
 
     def __str__(self):
         return f"{self.title}"
