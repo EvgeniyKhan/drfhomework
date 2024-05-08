34bedf7b481072f90d723d8748f15d080ad592c8
evgeniy <evgeniy.igorevich9402@gmail.com>
2024-05-09 00:08:14 +0500

    описал доступ

--- users/views.py
+++ users/views.py
@@ -1,8 +1,10 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, generics
 from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 from users.models import User, Payments
+from users.permissions import IsModerator
 from users.serliazers import UserSerializer, PaymentSerializer
 
 
@@ -12,14 +14,32 @@ class UserViewSet(viewsets.ModelViewSet):
 
 
 class UserCreateAPIView(generics.CreateAPIView):
-    """Создание пользователя"""
     serializer_class = UserSerializer
     queryset = User.objects.all()
+    permission_classes = [AllowAny]
+
+    def perform_create(self, serializer):
+        user = serializer.save(is_active=True)
+        user.set_password(user.password)
+        user.save()
 
 
 class UserListAPIView(generics.ListAPIView):
-    model = User
+    serializer_class = UserSerializer
     queryset = User.objects.all()
+    permission_classes = [IsAuthenticated]
+
+
+class UserUpdateAPIView(generics.UpdateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [IsModerator]
+
+
+class UserDeleteAPIView(generics.DestroyAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [IsModerator]
 
 
 class PaymentsCreateAPIView(generics.CreateAPIView):
