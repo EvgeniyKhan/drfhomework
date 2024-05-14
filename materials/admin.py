79ef1701d90dc16744660c390418f3f7f37fbbee
evgeniy <evgeniy.igorevich9402@gmail.com>
2024-05-14 23:44:44 +0500

    добавил админку

--- materials/admin.py
+++ materials/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from materials.models import Course, Lesson, Subscription
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ("title", "description", "preview")
+    search_fields = ("title",)
+
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ("title", "description", "preview", "course", "video_link")
+    search_fields = ("title",)
+
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ("user", "course")
+    search_fields = ("user",)
